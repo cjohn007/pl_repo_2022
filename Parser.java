ffbf6ebd36d7058033c5f119cc6637b1e74e6274
Carl John Galvan <carl.galvan@neu.edu.ph>
2022-10-23 18:04:49 +0800

    Lab_activity_4-original_released

---
+++ Parser.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Parser 
+{
+    public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please input syntax: ");
+		String input = scan.nextLine();
+		String result=doesSyntaxCorrect(input);
+		System.out.println(result);
+		
+	}
+    public static String doesSyntaxCorrect(String input)
+	{
+		String result="";
+		String[] correctSyntax= {"<data_type><identifier><assignment_operator><value><delimiter>","<data_type><identifier><delimiter>"};
+		input = input.replaceAll(" ", "");
+		for(String r:correctSyntax)
+		{
+			if(input.equals(r))
+			{
+				result="Syntax is correct!";
+				break;
+			}
+			else
+			{
+				result="Syntax is Error!";
+			}
+		}
+		return result;
+	}
+}
