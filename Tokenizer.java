180888bd2d668390510af3ae83f9b64f922531b7
cjohn007 <112838639+cjohn007@users.noreply.github.com>
2022-11-04 17:25:59 +0800

    Update Tokenizer in distinguishing cases

--- Tokenizer.java
+++ Tokenizer.java
@@ -3,63 +3,76 @@ import java.util.Scanner;
 
 public class Tokenizer 
 {
+	private static ArrayList<String> coll_lexemes;
+	private static char doub_quote = '"';
+	private static String single_quote="'";
+	private static ArrayList<String> dataTypes= new ArrayList<String>();
+	private static String numbers="0123456789";
     
     public static void main(String[] args) 
 	{
+		dataTypes.add("int");
+		dataTypes.add("double");
+		dataTypes.add("String");
+		dataTypes.add("char");
+		dataTypes.add("boolean");
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Please input code: ");
-		String input = scan.nextLine();
-		
-		//return token
-		String arr_token = tokenizer(input);
-		
-		System.out.println(arr_token);
+		coll_lexemes = new ArrayList<String>();
+		boolean run = true;
+		while(run==true)
+		{
+			System.out.println("Please input code: ");
+			String input = scan.nextLine();
+			//return token
+			String arr_token = tokenize(input);
+			System.out.println(arr_token);
+
+			//clear the List
+			coll_lexemes.clear();
+			
+			run = Boolean.valueOf(scan.nextLine());
+		}
+
 	}
 
-    public static String tokenizer(String input)
+	public static String tokenize(String input)
 	{
-		String arr_token = "";
-		String lexeme = "";
-		ArrayList<String> coll_lexemes = new ArrayList<String>();
-		String dataTypes = "int boolean String char double";
+		String tokens = "";
+
+		//separate lexeme then store it in coll_lexeme
+		coll_lexemes=splitLexemes(coll_lexemes,input);
+		//match every lexeme to corresponding token
+		tokens = matchLexemes(coll_lexemes);
 		
-		//separate lexemes then store it in coll_lexeme
-		for(int x = 0;x<input.length();x++)
+		
+		return tokens;
+	}
+	public static ArrayList<String> splitLexemes(ArrayList<String> coll_lexemes, String input)
+	{
+		String lexeme="";
+		
+		for(int x=0;x<input.length();x++)
 		{
-			char each_char = input.charAt(x);
-			String str_char = Character.toString(each_char); 
-			
-			if(each_char == ';')
+			String temp = Character.toString(input.charAt(x)); 
+
+			//if temp is equal to double or single quotation
+			if(input.charAt(x) == '"' || temp.equals("'"))
 			{
-				if(lexeme.isEmpty()==false)
-				{
-					coll_lexemes.add(lexeme);
-					coll_lexemes.add(";");
-				}
-				else
-				{
-					coll_lexemes.add(";");
-				}
-				
-			}
-			else if(each_char == '=')
-			{
-				coll_lexemes.add("=");
-			}
-			else if(each_char == '"')
-			{
-				String tempStr = "";
-				int count = 1;
+				int count = 1;//determine how many quotations
+				String tempStr="";
+				/*Loop to store the String values*/
 				for(int y=x;y<input.length();y++)
 				{
 					char input_char = input.charAt(y);
-					if(input_char == '"' && count==1)
+
+					//if it is the first double or single quotation 
+					if((input_char == '"'|| Character.toString(input_char).equals("'")) && count==1)
 					{
 						count++;
 						tempStr+= Character.toString(input_char);
 					}
-					else if (input_char == '"' && count==2)
+					//if it is the second double or single quotation 
+					else if ((input_char == '"'|| Character.toString(input_char).equals("'")) && count==2)
 					{
 						count++;
 						tempStr = tempStr+Character.toString(input_char);
@@ -72,30 +85,72 @@ public class Tokenizer
 						tempStr+=Character.toString(input_char);
 					}
 				}
+				lexeme="";
 				
 			}
-			else if(each_char != ' ')
+			//if temp is equal to assignment operator
+			else if(temp.equals("="))
 			{
-				if(x==input.length()-1)
+				setColl_Lexemes(lexeme);
+				coll_lexemes.add("=");
+				lexeme="";
+			}
+			//if temp is equal to semi-colon
+			else if(temp.equals(";"))
+			{
+				setColl_Lexemes(lexeme);
+				coll_lexemes.add(";");
+				lexeme="";
+			}
+			//if x is at the last character of input
+			else if(x==input.length()-1)
+			{
+				lexeme+=temp;
+				setColl_Lexemes(lexeme);
+			}
+			//if lexeme is within the dataTypes
+			//store the lexeme
+			else if(dataTypes.contains(lexeme)&& input.charAt(x+1)==' ') 
+			{
+				setColl_Lexemes(lexeme);
+				lexeme="";
+			}
+			
+			//if lexeme is an identifier
+			else if(lexeme.isEmpty()==false && (temp.equals(" ")))
+			{
+				setColl_Lexemes(lexeme);
+				lexeme="";
+			}
+			else
+			{
+			
+				if(temp.equals(" "))
 				{
-					lexeme+=str_char;
-					coll_lexemes.add(lexeme);
+					
 				}
 				else
 				{
-					lexeme+=str_char;
+					lexeme+=temp;
 				}
-			}
-			else if(each_char ==' ' && lexeme.isEmpty()==false)
-			{
+			
 				
-				coll_lexemes.add(lexeme);
-				lexeme="";
 			}
 		}
-		
-		System.out.println(coll_lexemes);
-		//Identify the lexeme
+		return coll_lexemes;
+	}
+	public static void setColl_Lexemes(String lexeme)
+	{
+		if(lexeme.isEmpty()==false)
+		{
+			coll_lexemes.add(lexeme);
+		}
+	}
+	
+	public static String matchLexemes(ArrayList<String> coll_lexemes)
+	{
+		String arr_token="";
+	
 		for(int y = 0;y<coll_lexemes.size();y++)
 		{
 			String element = coll_lexemes.get(y);
@@ -114,43 +169,44 @@ public class Tokenizer
 			}
 			else
 			{
-				char double_quote = '"';
-				String dobQoute_str = Character.toString(double_quote);
-				char elem_char = element.charAt(0);
-				String numbers = "1234567890";
-				
-				
-				//int 
-				if(element.contains(Character.toString('"')))
-				{
-					arr_token+="<value>";
+				element = element.replaceAll(" ","");
 
-				}
-				//char
-				else if (element.contains("'"))
+				
+				//string value
+				//if the first index are double quotation
+				if(element.charAt(0)==doub_quote)//(element.substring(0, element.length()-1).equals(Character.toString('"')))
 				{
 					arr_token+="<value>";
 				}
-				//double
+				//if the first index is single quotation
+				else if(Character.toString(element.charAt(0)).equals(single_quote))
+				{
+					arr_token+="<value>";
+				}
+				//if a number is the first index
+				else if(numbers.contains(element))
+				{
+					arr_token+="<value>";
+				}
+				//if the element has dot
 				else if (element.contains("."))
 				{
 					arr_token+="<value>";
 				}
-				else if (numbers.contains(Character.toString(elem_char)))
+				else if(element.equalsIgnoreCase("true")||element.equalsIgnoreCase("false"))
 				{
 					arr_token+="<value>";
 				}
-				else 
+
+				else
 				{
 					arr_token+="<identifier>";
-				}
-				
-				
+				}		
 			}
 		}
 		
-		
-		
 		return arr_token;
-	}
+	}	
 }
+
+
